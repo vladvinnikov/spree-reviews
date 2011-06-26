af9e95f0c077225b05e4c3269cf88420ca032709
Roman Smirnov <roman@railsdog.com>
2011-06-26 22:30:31 +0400

    Switch to Deface

--- lib/spree_reviews_hooks.rb
+++ lib/spree_reviews_hooks.rb
@@ -1,24 +1,29 @@
-class SpreeReviewsHooks < Spree::ThemeSupport::HookListener
-  insert_after :product_properties, 'shared/reviews'
-  insert_after :inside_head do
-    %( <%= stylesheet_link_tag('reviews.css') %> )
-  end
-  
-  insert_after :footer_right do
-    %(
-    <%= javascript_include_tag("jquery.rating.js") %>
-    <%= javascript_tag("$(document).ready(function(){$('.stars').rating({required:true});});") %>
-    )
-  end
+Deface::Override.new(
+  :virtual_path => "layouts/spree_application",
+  :name => "reviews_inside_head",
+  :insert_bottom => "[data-hook='inside_head']",
+  :text => "<%= stylesheet_link_tag('reviews.css') %>",
+  :disabled => false)
 
-  insert_after :admin_product_sub_tabs do
-    %(
-       <%= tab(:reviews, :label => 'review_management') %>
-     )
-  end
+Deface::Override.new(
+  :virtual_path => "shared/_footer",
+  :name => "reviews_footer_right",
+  :insert_after => "[data-hook='footer_right']",
+  :text => "<%= javascript_include_tag('jquery.rating.js') %>
+  <%= javascript_tag(\"$(document).ready(function(){$('.stars').rating({required:true});});\") %>",
+  :disabled => false)
 
-  insert_after :admin_configurations_menu do
-    %(<%= configurations_menu_item(t('spree_reviews.review_settings'), admin_review_settings_path, t('spree_reviews.manage_review_settings')) %>)
-  end
+Deface::Override.new(
+  :virtual_path => "admin/shared/_product_sub_menu",
+  :name => "reviews_admin_product_sub_tabs",
+  :insert_bottom => "[data-hook='admin_product_sub_tabs']",
+  :text => "<%= tab(:reviews, :label => 'review_management') %>",
+  :disabled => false)
 
-end
+
+Deface::Override.new(
+  :virtual_path => "admin/configurations/index",
+  :name => "reviews_admin_configurations_menu",
+  :insert_bottom => "[data-hook='admin_configurations_menu']",
+  :text => "<%= configurations_menu_item(t('spree_reviews.review_settings'), admin_review_settings_path, t('spree_reviews.manage_review_settings')) %>",
+  :disabled => false)
