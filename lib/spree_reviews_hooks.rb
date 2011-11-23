52e5ad0f20cca9c845c79e828889590789908eb2
Vladimir Vinnikov <vladvinnikov@gmail.com>
2011-11-23 21:22:24 +0200

    Update lib/spree_reviews_hooks.rb

--- lib/spree_reviews_hooks.rb
+++ lib/spree_reviews_hooks.rb
@@ -1,13 +1,6 @@
 class SpreeReviewsHooks < Spree::ThemeSupport::HookListener
   insert_after :product_properties, 'shared/reviews'
 
-  
-  insert_after :footer_right do
-    %(
-    <%= javascript_include_tag("jquery.rating.js") %>
-    <%= javascript_tag("$(document).ready(function(){$('.stars').rating({required:true});});") %>
-    )
-  end
 
   insert_after :admin_product_sub_tabs do
     %(
